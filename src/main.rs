26dea69a6c382057feb6cf346581ac832e8f8504
Jim Crossley <jim@crossleys.org>
2022-05-06 13:36:44 -0400

    Bare minimum to find bluetooth adapter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> bluer::Result<()> {
+    env_logger::init();
+    let session = bluer::Session::new().await?;
+    let adapter = session.default_adapter().await?;
+    adapter.set_powered(true).await?;
+    println!(
+        "Bluetooth adapter {} with address {}",
+        adapter.name(),
+        adapter.address().await?
+    );
+    Ok(())
 }
